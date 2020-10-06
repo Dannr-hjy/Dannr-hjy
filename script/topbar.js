7738c8c639f2369cffd2c635f09063b98fbda792
Dannr-hjy <63134885+Dannr-hjy@users.noreply.github.com>
2020-10-06 18:15:16 +0800

    Add files via upload

---
+++ script/topbar.js
@@ -0,0 +1,9 @@
+document.write(`
+<div class="mdui-toolbar mdui-color-theme mdui-shadow-3">
+<i class="mdui-icon material-icons">&#xe80c;</i>
+<span class="mdui-typo-title">师大滨江C2004班</span>
+  <div class="mdui-toolbar-spacer"></div>
+<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>
+<a href="login/login.html">登录</a>
+</div>
+`);
