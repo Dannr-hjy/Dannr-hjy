7b1a03f990a0cadf88cfa6470943680495cf7eda
Dannr-hjy <63134885+Dannr-hjy@users.noreply.github.com>
2020-10-06 18:25:12 +0800

    Update topbar.js

--- script/topbar.js
+++ script/topbar.js
@@ -3,7 +3,6 @@ document.write(`
 <i class="mdui-icon material-icons">&#xe80c;</i>
 <span class="mdui-typo-title">师大滨江C2004班</span>
   <div class="mdui-toolbar-spacer"></div>
-<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>
 <a href="login/login.html">登录</a>
 </div>
 `);
