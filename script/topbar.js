32264d0f0770c95c812ec2b47fcc68c1727973e9
Dannr-hjy <63134885+Dannr-hjy@users.noreply.github.com>
2020-12-05 20:22:58 +0800

    Update topbar.js

--- script/topbar.js
+++ script/topbar.js
@@ -1,8 +1,15 @@
 document.write(`
-<div class="mdui-toolbar mdui-color-theme mdui-shadow-3">
-<i class="mdui-icon material-icons">&#xe80c;</i>
-<span class="mdui-typo-title">师大滨江C2004班</span>
-  <div class="mdui-toolbar-spacer"></div>
-<a href="login/login.html">登录</a>
+<div class="mdui-appbar">
+  <div class="mdui-toolbar mdui-color-indigo">
+    <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">menu</i></a>
+    <a href="javascript:;" class="mdui-typo-title">师大滨江C2004班</a>
+    <div class="mdui-toolbar-spacer"></div>
+    <a href="login/login.html">登录</a>
+  </div>
+  <div class="mdui-tab mdui-color-indigo" mdui-tab>
+    <a href="https://www.sdbjc2004.tk/" class="mdui-ripple mdui-ripple-white">首页</a>
+    <a href="https://www.sdbjc2004.tk/news/all.html" class="mdui-ripple mdui-ripple-white">新闻</a>
+    <a href="#example3-tab1" class="mdui-ripple mdui-ripple-white">题库</a>
+  </div>
 </div>
 `);
